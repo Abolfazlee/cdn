b0b97de83063acdae638ec1ff28c062c245be659
Abolfazlee <117379230+Abolfazlee@users.noreply.github.com>
2023-04-24 12:40:46 +0330

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function() {
+    $('#myForm').on('submit', function(event) {
+        event.preventDefault();
+        var iId = $('#iId').val();
+        $.ajax({
+            type: 'POST',
+            url: 'index.php',
+            data: { iId: iId },
+            success: function(response) {
+                $('#result').html(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+});
